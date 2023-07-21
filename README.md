5758a36728030dc9c1fc6227426c74b7cbe3437c
giomascitelli <47045018+giomascitelli@users.noreply.github.com>
2023-07-21 21:07:08 +0000

    This is the final project that I completed as part of the 'Scientific Computing with Python' course from freeCodeCamp.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Probability Calculator
 
-This is the boilerplate for the Probability Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/probability-calculator
+This is the final project that I completed as part of the 'Scientific Computing with Python' course from freeCodeCamp. For this project, I worked on Replit using their test code to develop a class and a function. The Hat class takes a variable number of arguments that specify the number of balls of each color inside the hat. The arguments passed into the Hat are converted into a list, showing the color name and the corresponding number of balls inside the hat.
+
+The Hat class also has a method called 'draw', which accepts an argument indicating the number of balls to draw from the hat. This method randomly removes balls from the list and returns those balls as a list of strings.
+
+The experiment function accepts several variables to calculate the probability of a specific experiment occurring based on the number of balls inside the hat.
--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,43 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
-
+    # Class hat that can take any number of arguments
+    def __init__(self, **kwargs):
+        self.contents = []
+        
+        for color, count in kwargs.items():
+            self.contents.extend([color] * count)
+        
+    def draw(self, num_balls):
+        num_balls = min(num_balls, len(self.contents))
+        drawn_balls = random.sample(self.contents, num_balls)
+        
+        for ball in drawn_balls:
+            self.contents.remove(ball)
+        return drawn_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    successful_experiments = 0
+
+    # Creating a loop to iterate over the number of experiments and the copy of the balls inside the hat
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+        
+        success = True
+
+        # Creating a loop to iterate over the expected balls input and if the drawn balls are less than the expected, the loop breaks as the it is unsuccessful. If it is successful, then we add 1 to the 'successful_experiments' variable so that we can calculate the probability in the end
+        for color, count in expected_balls.items():
+            if drawn_balls.count(color) < count:
+                success = False
+                break
+        
+        if success:
+            successful_experiments += 1
+            
+    probability = successful_experiments / num_experiments
+    
+    return probability
+
